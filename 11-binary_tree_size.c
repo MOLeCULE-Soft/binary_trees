d117c5e93440f830a016576a68caae006ce2ab59
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 22:18:03 +0100

    Task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,13 +9,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 1;
+	size_t size = 0;
 
 	if (tree)
 	{
+		size++;
 		size += binary_tree_size(tree->left);
 		size += binary_tree_size(tree->right);
 	}
 
-	return (--size);
+	return (size);
 }
