273ba9e40c5b3b09c1b49649724a382809883b99
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 22:33:26 +0100

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,35 +4,34 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int is_full_recursive(const binary_tree_t *tree);
 
 /**
- * binary_tree_is_full - Checks if a binary tree is full.
+ * binary_tree_is_full - checks if binary tree is full
  *
- * @tree: A pointer to the root node of the tree to check.
+ * @tree: pointer
  *
- * Return: 0 if tree is NULL.
+ * Return: 0 if tree is NULL
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 	return (is_full_recursive(tree));
 }
 
 /**
- * is_full_recursive - Checks if a binary tree is full recursively.
+ * is_full_recursive - checks if a binary tree is full recursively
  *
- * @tree: A pointer to the root node of the tree to check.
+ * @tree: pointer
  *
- * Return: 0 if tree is not NULL, otherwise 1.
+ * Return: 0 if tree is not NULL, otherwise 1
  */
 int is_full_recursive(const binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree)
 	{
-		if ((tree->left != NULL && tree->right == NULL) ||
-		    (tree->left == NULL && tree->right != NULL) ||
+		if ((tree->left && !tree->right) ||
+		    (!tree->left && tree->right) ||
 		    is_full_recursive(tree->left) == 0 ||
 		    is_full_recursive(tree->right) == 0)
-
 			return (0);
 	}
 
