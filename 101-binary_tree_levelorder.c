33262b33b8e58abcb7daf603d1b3a01b1131328f
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 23:03:17 +0100

    Taskn 20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -17,14 +17,14 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	levelorder_queue_t *head, *tail;
 
-	if (tree == NULL || func == NULL)
+	if (!tree || !func)
 		return;
 
 	head = tail = create_node((binary_tree_t *)tree);
-	if (head == NULL)
+	if (!head)
 		return;
 
-	while (head != NULL)
+	while (head)
 	{
 		pint_push(head->node, head, &tail, func);
 		pop(&head);
@@ -44,23 +44,20 @@ levelorder_queue_t *create_node(binary_tree_t *node)
 	levelorder_queue_t *new;
 
 	new = malloc(sizeof(levelorder_queue_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->node = node;
-	new->next = NULL;
-
+	if (new)
+	{
+		new->node = node;
+		new->next = NULL;
+	}
 	return (new);
 }
 
 /**
- * pint_push - Runs a function on a given binary tree node and
- *             pushes its children into a levelorder_queue_t queue.
- *
- * @node: The binary tree node to print and push.
- * @head: A double pointer to the head of the queue.
- * @tail: A double pointer to the tail of the queue.
- * @func: A pointer to the function to call on @node.
+ * pint_push - pint_push
+ * @node: pointer
+ * @head: pointer
+ * @tail: pointer
+ * @func: pointer
  *
  * Description: Upon malloc failure, exits with a status code of 1.
  */
@@ -70,10 +67,10 @@ void pint_push(binary_tree_t *node, levelorder_queue_t *head,
 	levelorder_queue_t *new;
 
 	func(node->n);
-	if (node->left != NULL)
+	if (node->left)
 	{
 		new = create_node(node->left);
-		if (new == NULL)
+		if (!new)
 		{
 			free_queue(head);
 			exit(1);
@@ -81,10 +78,10 @@ void pint_push(binary_tree_t *node, levelorder_queue_t *head,
 		(*tail)->next = new;
 		*tail = new;
 	}
-	if (node->right != NULL)
+	if (node->right)
 	{
 		new = create_node(node->right);
-		if (new == NULL)
+		if (!new)
 		{
 			free_queue(head);
 			exit(1);
@@ -103,7 +100,7 @@ void free_queue(levelorder_queue_t *head)
 {
 	levelorder_queue_t *tmp;
 
-	while (head != NULL)
+	while (head)
 	{
 		tmp = head->next;
 		free(head);
