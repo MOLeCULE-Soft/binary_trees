14f5e241cb95f7f59b8b5672782576c2595500b6
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 21:56:30 +0100

    Task 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new = binary_tree_node(parent, value);
 	if (new)
 	{
-		if (parent->left != NULL)
+		if (parent->left)
 		{
 			new->left = parent->left;
 			parent->left->parent = new;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new = binary_tree_node(parent, value);
 	if (new)
 	{
-		if (parent->right != NULL)
+		if (parent->right)
 		{
 			new->right = parent->right;
 			parent->right->parent = new;
