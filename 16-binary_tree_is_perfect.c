d0b4ae85fd972258e496f61140e42a0931bb3091
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 22:46:45 +0100

    Taskn 17

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,9 +33,9 @@ size_t depth(const binary_tree_t *tree)
 
 /**
  * get_leaf - Returns a leaf of a binary tree.
- * @tree: A pointer to the root node of the tree to find a leaf in.
+ * @tree: pointer
  *
- * Return: A pointer to the first encountered leaf.
+ * Return: pointer to the first leaf encountered
  */
 const binary_tree_t *get_leaf(const binary_tree_t *tree)
 {
@@ -45,19 +45,19 @@ const binary_tree_t *get_leaf(const binary_tree_t *tree)
 }
 
 /**
- * is_perfect_recursive - Checks if a binary tree is perfect recursively.
- * @tree: A pointer to the root node of the tree to check.
- * @leaf_depth: The depth of one leaf in the binary tree.
- * @level: Level of current node.
+ * is_perfect_recursive - checks if a binary tree is perfect recursively
+ * @tree: pointer
+ * @leaf_depth: depth of one leaf in binary tree
+ * @level: current node level
  *
- * Return: If the tree is perfect, 1, otherwise 0.
+ * Return: ff the tree is perfect, 1, otherwise 0
  */
 int is_perfect_recursive(const binary_tree_t *tree,
 		size_t leaf_depth, size_t level)
 {
 	if (is_leaf(tree))
 		return (level == leaf_depth ? 1 : 0);
-	if (tree->left == NULL || tree->right == NULL)
+	if (!tree->left || !tree->right)
 		return (0);
 	return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
 		is_perfect_recursive(tree->right, leaf_depth, level + 1));
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,18 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - Finds the sibling of a node.
+ * binary_tree_sibling - finds sibling node
+ * @n: pointer
  *
- * @node: A pointer to the node to find the sibling.
- *
- * Return: NULL if node is NULL or parent is NULL or node has no sibiling.
+ * Return: sibling otherwise, NULL
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *n)
 {
-	if (node == NULL || node->parent == NULL)
+	if (!n || !n->parent)
 		return (NULL);
-	if (node->parent->left == node)
-		return (node->parent->right);
-
-	return (node->parent->left);
+	return (n->parent->left == n ? n->parent->right : n->parent->left);
 }
