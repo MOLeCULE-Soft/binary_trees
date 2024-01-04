589a11b67b6ffd5045889acac794f4de2542b13a
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 22:11:12 +0100

    Task 9

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
+ * binary_tree_preorder - pre-order traversal
  *
- * @tree: A pointer to the root node of the tree to traverse.
- * @func: A pointer to a function to call for each node.
+ * @tree: pointer
+ * @func: pointer
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_height - measures binary tree height
  *
- * @tree: A pointer to the root node of the tree to measure the height.
+ * @tree: pointer
  *
- * Return: 0 if tree is NULL.
+ * Return: 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t lef = 0, rig = 0;
+		size_t left = 0, right = 0;
 
-		lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-		return ((lef > rig) ? lef : rig);
+		return (left > right ? left : right);
 	}
 
 	return (0);
