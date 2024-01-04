2a7a08f220e8b877078a7e4d3639440310ed42f4
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-04 23:20:58 +0100

    Task 113

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,24 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * bst_search - Searches for a value in a Binary Search Tree.
+ * bst_search - bst_search
+ * @tree: pointer
+ * @value: to be searched
  *
- * @tree: A pointer to the root node of the BST to search.
- * @value: The value to search in the tree.
- *
- * Return: A pointer to the node containing a value equals to value.
+ * Return:  pointer to the node containing a value equals to value
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-	if (tree != NULL)
+	if (tree)
 	{
 		if (tree->n == value)
 			return ((bst_t *)tree);
 		if (tree->n > value)
 			return (bst_search(tree->left, value));
-
 		return (bst_search(tree->right, value));
 	}
-
 	return (NULL);
 }
